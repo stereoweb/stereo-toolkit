4598f5edc0791ba5a0c0f9bcae9f01071934427f
Jonathan Grenier <jonathan@stereo.ca>
2022-09-09 14:49:13 -0400

    Change version

--- stereo-toolkit.php
+++ stereo-toolkit.php
@@ -4,7 +4,7 @@
  * Description: Stereo plugin to handle redirections, site tags & more
  * Author: Stereo
  * Author URI: https://www.stereo.ca/
- * Version: 1.1.0
+ * Version: 1.1.1
  * License:     0BSD
  *
  * Copyright (c) 2022 Stereo
@@ -23,7 +23,7 @@ if (!class_exists('ST_Toolkit')) {
 
     class ST_Toolkit
     {
-        var $version = "1.1.0";
+        var $version = "1.1.1";
 
         public function __construct()
         {
